368ab2ea19c234d80da649804a7c126b28c5223d
GerritHillebrecht <gerrit.hillebrecht@gmail.com>
2025-08-06 11:51:10 +0200

    feat(header): refactor Header component to include Notifications and Messages

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,16 @@
+import Messages from './Messages';
+import Notifications from './Notificatons';
+
 export function Header() {
   return (
     <header className="w-full">
       <nav className="container mx-auto flex items-center justify-between px-4 py-3">
-        <h1 className="text-xl font-bold">Instagram</h1>
-        <div className="text-xl">❤️</div>
+        {/* <h1 className="text-xl font-bold">Instagram</h1> */}
+        <img src="https://www.pngkey.com/png/full/2-28310_instagram-logo-black-and-ahite-instagram-word-logo.png" alt="Instagram Logo" className="h-8 -mb-0 mt-1" />
+        <div className="text-xl flex items-center gap-x-0">
+          <Notifications />
+          <Messages />
+        </div>
       </nav>
     </header>
   );
